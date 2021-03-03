5656f61cae2ca0569f8edf0a92dffa8cf694e7cd
Mitra Nami <49569563+MitraNami@users.noreply.github.com>
2021-03-03 15:12:54 -0500

    Update fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -34,3 +34,64 @@ const pageDownloader = function(url, filePath) {
 
 const [url, localPath] = process.argv.slice(2);
 pageDownloader(url,localPath);
+
+const request = require('request');
+// const fs = require('fs');
+// const readline = require('readline');
+
+
+// const write = function(localPath, body) {
+//   fs.writeFile(localPath, body, err => {
+//     if (err) {
+//       // localPath is invalid
+//       console.log(`Path ${localPath} doesn't exist.`);
+//       return;
+//     }
+//     console.log(`Downloaded and saved ${body.length} bytes to ${localPath}`)
+//   });
+// }
+
+
+// const fetcheAndSave = function(url, localPath) {
+
+//   request(url, (err, res, body) => {
+//     if (err) {
+//       console.log('Error', err);
+//       return;
+//     }
+//     if (res.statusCode !== 200) {
+//       console.log(`Status Code: ${res.statusCode}`);
+//       return;
+//     }
+//     // no Error and res.statusCode is 200
+    
+//     fs.access(localPath, err => {
+//       if (err) {
+//         //localPath doesn't exist
+//         write(localPath, body);
+
+//       } else {
+//         // localPath exists, Ask the user if they want to replace the file
+//         const rl = readline.createInterface({
+//           input: process.stdin,
+//           output: process.stdout
+//         });
+//         rl.question('The file exists, do you want to replace it? (y/n) ', ans => {
+//           if (ans === 'y') {
+//             write(localPath, body);
+//           }
+//           rl.close();
+//         });
+//       }
+//     });
+//   });
+// };
+
+
+// const url = process.argv[2];
+// const localPath = process.argv[3];
+// if (url && localPath) {
+//   fetcheAndSave(url, localPath);
+// } else {
+//   console.log('You must provide both a url and localpath');
+// }
